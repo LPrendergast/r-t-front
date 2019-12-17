5bfb95b7ea035bae9e2956b8d4fa39f0f8492a55
Luke Prendergast <luke.prendergast94@gmail.com>
2019-12-17 17:07:45 +0000

    added map functionality , responds to lat and lon after creating event and loads once event is loaded

--- src/pages/events/Map.js
+++ src/pages/events/Map.js
@@ -1,40 +1,77 @@
-import React from "react"
-import { compose, withProps, lifecycle } from "recompose"
-import { DirectionsRenderer, withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+// import React from "react"
+// import { compose, withProps, lifecycle } from "recompose"
+// import { DirectionsRenderer, withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+// import GoogleMapReact from 'google-map-react';
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAcSmT9EYjDiK4u_1NyVGayyvPMiomka2c&libraries=places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-  mapElement: <div style={{ height: `100%`}} />
-  }),
-  withScriptjs,
-  withGoogleMap,)(props =>
-  <GoogleMap
-    defaultZoom={12}
-    defaultCenter={new window.google.maps.LatLng(0,0)}
-  >
-  <Marker position={{ lat: 0, lng: 0}} />
-  </GoogleMap>
-);
 
-export default class Map extends React.PureComponent {
-  state = {
-    isMarkerShown: true
-  }
+// const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
+//
+// const MapWithAMarkerWithLabel = compose(
+//   withScriptjs,
+//   withGoogleMap
+// )(props =>
+//   <GoogleMap
+//     defaultZoom={8}
+//     defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//   >
+//     <MarkerWithLabel
+//       position={{ lat: -34.397, lng: 150.644 }}
+//       labelAnchor={new window.google.maps.Point(0, 0)}
+//       labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+//     >
+//       <div>Hello There!</div>
+//     </MarkerWithLabel>
+//   </GoogleMap>
+// );
+//
+// export default class Map extends React.PureComponent {
+//   state = {
+//     isMarkerShown: true,
+//     latitude: this.props.latitude,
+//     longitude: this.props.longitude
+//   }
+//
+//   render() {
+//     return (
+//       <div style={{ height: '100vh', width: '100%' }}>
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: 'add your api key' }}
+//           defaultCenter={this.state.latitude, this.state.longitude}
+//           defaultZoom={12}
+//         >
+//
+//         </GoogleMapReact>
+//       </div>
+//     )
+//   }
+// }
 
-  render() {
+
+
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker';
+
+
+const SimpleMap = (props: any) => {
+    const [center, setCenter] = useState({lat: props.latitude, lng: props.longitude });
+    const [zoom, setZoom] = useState(13);
     return (
-      <MyMapComponent
-        origin={this.props.origin}
-        destination={this.props.destination}
-        selectedTransportMode={this.props.selectedTransportMode}
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-        latitude={this.props.latitude}
-        longitude={this.props.longitude}
-      />
-    )
-  }
+        <div style={{ height: '75vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAcSmT9EYjDiK4u_1NyVGayyvPMiomka2c' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <Marker
+            lat={props.latitude}
+            lng={props.longitude}
+            name="My Marker"
+            color="blue"
+          />
+        </GoogleMapReact>
+      </div>
+    );
 }
+
+export default SimpleMap;
